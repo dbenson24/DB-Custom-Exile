d84ed82f334d857afc039b8e64343e28d4d9fe81
dbenson24 <derekbenson24@mgail.com>
2015-09-04 16:10:33 -0400

    Copy instead of move, make script

--- scripts/make.bat
+++ scripts/make.bat
@@ -10,8 +10,8 @@ call %PBO% %Server%a3_dms
 
 call %PBO% %ProjectHome%\mpmissions\Exile.Altis
 
-move %Server%exile_server.pbo %ARMA%@ExileServer\addons\exile_server.pbo"
-move %Server%exile_server_config.pbo %ARMA%@ExileServer\addons\exile_server_config.pbo"
-move %Server%a3_dms.pbo %ARMA%@ExileServer\addons\a3_dms.pbo"
+copy %Server%exile_server.pbo %ARMA%@ExileServer\addons\exile_server.pbo"
+copy %Server%exile_server_config.pbo %ARMA%@ExileServer\addons\exile_server_config.pbo"
+copy %Server%a3_dms.pbo %ARMA%@ExileServer\addons\a3_dms.pbo"
 
-move %ProjectHome%\mpmissions\Exile.Altis.pbo %ARMA%\mpmissions\Exile.Altis.pbo"
+copy %ProjectHome%\mpmissions\Exile.Altis.pbo %ARMA%\mpmissions\Exile.Altis.pbo"
