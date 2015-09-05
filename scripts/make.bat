c337f629a1eeb0844cc333b11ea348ba2bfd03d3
Derek Benson <derek.benson@tufts.edu>
2015-09-05 09:16:28 -0400

    Test Commit

--- scripts/make.bat
+++ scripts/make.bat
@@ -10,6 +10,8 @@ call %PBO% %Server%a3_dms
 
 call %PBO% %ProjectHome%\mpmissions\Exile.Altis
 
+
+
 copy %Server%exile_server.pbo %ARMA%@ExileServer\addons\exile_server.pbo"
 copy %Server%exile_server_config.pbo %ARMA%@ExileServer\addons\exile_server_config.pbo"
 copy %Server%a3_dms.pbo %ARMA%@ExileServer\addons\a3_dms.pbo"
